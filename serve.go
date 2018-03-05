60ce58561ba32e7becc8fe1299bd97493d5ae3e3
denis zaytcev <d.zaycev@software2life.com>
2018-03-05 09:28:35 +0300

    Сервер для отдачи статики

--- serve.go
+++ serve.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
+	"time"
+	"flag"
+	"fmt"
 )
 
 func main() {
-  fs := http.FileServer(http.Dir("build"))
-  http.Handle("/", fs)
+	p := flag.Int("p", 80, "Port")
+	flag.Parse()
 
-  log.Println("Listening 0.0.0.0:80")
-  http.ListenAndServe(":80", nil)
+	r := http.NewServeMux()
+	r.Handle("/", http.FileServer(http.Dir("build")))
+	r.Handle("*", http.FileServer(http.Dir("build/index.html")))
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", *p),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Printf("Listening 0.0.0.0:%d \n", *p)
+	log.Fatal(s.ListenAndServe())
 }
+
