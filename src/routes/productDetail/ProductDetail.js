eb38f30b928545010323bdec2eb0d11f31d58e95
denis zaytcev <d.zaycev@software2life.com>
2018-04-09 21:54:45 +0300

    Эффекты

--- src/routes/productDetail/ProductDetail.js
+++ src/routes/productDetail/ProductDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StoreComponent } from 'modules/Flux';
+import {StoreComponent} from 'modules/Flux';
 import Product from 'flux/Product';
 import Notation from 'flux/Notation';
 import Actions from 'flux/CartActions';
@@ -8,12 +8,13 @@ import Button from 'components/Button';
 import Title from 'components/Title';
 import Image from 'components/Image';
 import Range from 'components/Range';
+import List from 'components/List';
 import Currency from 'components/Currency';
-import { toast } from 'react-toastify';
-import { css } from 'glamor';
+import {toast} from 'react-toastify';
+import {css} from 'glamor';
 import './ProductDetail.css';
 
-const defaultText = "Нет описания";
+const defaultText = 'Нет описания';
 
 export default class ProductDetail extends StoreComponent {
   getInitialState() {
@@ -24,31 +25,31 @@ export default class ProductDetail extends StoreComponent {
 
   retrieveStoreData(store, attrs) {
     if (store.id === 'product')
-      return { product: attrs };
+      return {product: attrs};
 
     if (store.id === 'notation')
-      return { notation: attrs }
+      return {notation: attrs};
   }
 
   getInitialStore() {
-    return [ Product, Notation ];
+    return [Product, Notation];
   }
 
   handleCart = async () => {
-    const { amount, product } = this.state;
+    const {amount, product} = this.state;
     try {
       await Actions.insert(product, amount);
-      const { name } = product;
+      const {name} = product;
       toast.info(`${name} добавлен в корзину покупок!`, {
         hideProgressBar: true,
         position: toast.POSITION.TOP_CENTER,
         autoClose: 2000,
         className: css({
           top: 40,
-          minHeight: 40
-        })
+          minHeight: 40,
+        }),
       });
-    } catch(e) {
+    } catch (e) {
       console.log(e);
       toast.error(`Ошибка при работе с корзиной`, {
         autoClose: 2000,
@@ -56,8 +57,8 @@ export default class ProductDetail extends StoreComponent {
         position: toast.POSITION.TOP_CENTER,
         className: css({
           top: 40,
-          minHeight: 40
-        })
+          minHeight: 40,
+        }),
       });
     }
   };
@@ -65,99 +66,109 @@ export default class ProductDetail extends StoreComponent {
   render() {
     const {
       amount,
-      product: { name, discount, quantity, pictures, price, form, factor, producer },
+      product: {name, discount, quantity, pictures, price, form, factor, producer},
       notation: {
         research,
         prescribing,
         description,
-        composition },
+        effects,
+        composition,
+      },
     } = this.state;
 
     console.log(prescribing);
 
     return (
-      <div className="Product-detail">
-        <div className="Product-detail__row">
-          <div className="Product-detail__view">
+        <div className="Product-detail">
+          <div className="Product-detail__row">
+            <div className="Product-detail__view">
+              <Carousel
+                  dots
+                  arrows={false}
+                  infinity={false}
+                  className="Product-detail__carousel">
+                {
+                  pictures.map((src, idx) => (
+                      <div key={idx} className="Product-detail__carousel-image">
+                        <Image src={src} placeholder={'no_photo.jpg'}/>
+                      </div>
+                  ))
+                }
+              </Carousel>
+            </div>
+            <div className="Product-detail__info">
+              <div className="Product-detail__name">
+                {name}
+              </div>
+              <div className="Product-detail__producer">
+                {producer}
+              </div>
+              <div>
+                <div className="Product-detail__factor">
+                  {factor}
+                </div>
+                <div className="Product-detail__form">
+                  {form}
+                </div>
+              </div>
+              <div className="Product-detail__price">
+                <Currency value={price} discount={discount}/>
+              </div>
+              <div className="Product-detail__range">
+                <Range
+                    value={amount}
+                    onChange={(a) => {
+                      this.setState({amount: a});
+                    }}
+                    max={quantity}
+                />
+              </div>
+              <Button onClick={this.handleCart}
+                      className="Product-detail__cart">
+                В корзину
+              </Button>
+            </div>
+          </div>
+          <div className="Product-detail__row">
             <Carousel
-              dots
-              arrows={false}
-              infinity={false}
-              className="Product-detail__carousel">
+                dots
+                arrows={false}
+                infinity={false}
+                adaptiveHeight
+                className="Product-detail__carousel_texts">
               {
-                pictures.map((src, idx)=>(
-                  <div key={idx} className="Product-detail__carousel-image">
-                    <Image src={src} placeholder={"no_photo.jpg"} />
-                  </div>
+                [
+                  {
+                    title: 'Описание',
+                    body: description || defaultText,
+                  },
+                  effects
+                      ? {
+                        title: 'Эффекты',
+                        body: <List text={effects} />,
+                      }
+                      : {
+                        title: 'Исследования',
+                        body: research || defaultText,
+                      },
+                  {
+                    title: 'Рекомендации',
+                    body: prescribing || defaultText,
+                  },
+                  {
+                    title: 'Состав',
+                    body: composition || defaultText,
+                  },
+                ].map(({body, title}, idx) => (
+                    <div key={idx} className="Product-detail__text">
+                      <Title>{title}</Title>
+                      <div>{body}</div>
+                    </div>
                 ))
               }
             </Carousel>
           </div>
-          <div className="Product-detail__info">
-            <div className="Product-detail__name">
-              {name}
-            </div>
-            <div className="Product-detail__producer">
-              {producer}
-            </div>
-            <div>
-              <div className="Product-detail__factor">
-                {factor}
-              </div>
-              <div className="Product-detail__form">
-                {form}
-              </div>
-            </div>
-            <div className="Product-detail__price">
-              <Currency value={price} discount={discount} />
-            </div>
-            <div className="Product-detail__range">
-              <Range
-                value={amount}
-                onChange={(a)=>{ this.setState({ amount: a })}}
-                max={quantity}
-              />
-            </div>
-            <Button onClick={this.handleCart} className="Product-detail__cart">
-              В корзину
-            </Button>
-          </div>
         </div>
-        <div className="Product-detail__row">
-          <Carousel
-            dots
-            arrows={false}
-            infinity={false}
-            adaptiveHeight
-            className="Product-detail__carousel_texts">
-            {
-              [
-                {
-                  title: 'Описание',
-                  body: description || defaultText,
-                },
-                {
-                  title: 'Исследования',
-                  body: research || defaultText,
-                },
-                {
-                  title: 'Рекомендации' ,
-                  body: prescribing || defaultText,
-                },
-                {
-                  title: 'Состав',
-                  body: composition || defaultText,
-                }
-              ].map(({ body, title}, idx)=>(
-                <div key={idx} className="Product-detail__text">
-                  <Title>{title}</Title>
-                  <div>{body}</div>
-                </div>
-              ))
-            }
-          </Carousel>
-        </div>
-      </div>
     );
   }
 }
