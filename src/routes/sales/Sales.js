78ae7642bcdfbdfeccb57d03b0b8d2ab78d42957
denis zaytcev <d.zaycev@software2life.com>
2018-04-10 11:18:39 +0300

    Бадже скидка

--- src/routes/sales/Sales.js
+++ src/routes/sales/Sales.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css } from 'glamor';
+import {css} from 'glamor';
 import history from 'core/history';
+import Sale from 'components/Sale';
 import Title from 'components/Title';
 import Actions from 'flux/CartActions';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 import QueryManager from 'flux/QueryManager';
-import { QueryComponent, PaginationView } from 'modules/QueryController';
+import {PaginationView, QueryComponent} from 'modules/QueryController';
 import SaleProduct from './SaleProduct';
-import SaleBadge from './SaleBadge';
 import './Sales.css';
 
 export default class Sales extends QueryComponent {
@@ -27,10 +27,10 @@ export default class Sales extends QueryComponent {
           position: toast.POSITION.TOP_CENTER,
           className: css({
             top: 40,
-            minHeight: 40
-          })
+            minHeight: 40,
+          }),
         });
-      } catch(e) {
+      } catch (e) {
         console.log(e);
         toast.error(`Ошибка при работе с корзиной`, {
           autoClose: 2000,
@@ -38,62 +38,69 @@ export default class Sales extends QueryComponent {
           position: toast.POSITION.TOP_CENTER,
           className: css({
             top: 40,
-            minHeight: 40
-          })
+            minHeight: 40,
+          }),
         });
       }
-    }
+    };
   }
 
   render() {
-    const { query } = this.state;
+    const {query} = this.state;
 
     return (
-      <div className="Sales">
-        <Title>Распродажа</Title>
-        <div className="Sales__table">
-          {
-            query.map(i => <div key={i.id} className="Sales__table-item">
-              <SaleBadge {...i.discount} className="Sales__table-item__badge"/>
-              <SaleProduct
-                name={i.name}
-                price={i.price}
-                producer={i.producer}
-                discount={i.discount}
-                pictures={i.pictures}
-                quantity={i.quantity}
-                onClick={()=>(history.push(`/product/${i.SKU}`))}
-                onAdd={this.handleAdd(i)}
-                className="Sales__table-item__product"
-              />
-            </div>)
-          }
-        </div>
-        <PaginationView query={query} className="Sales__pagination" />
+        <div className="Sales">
+          <Title>Распродажа</Title>
+          <div className="Sales__table">
+            {
+              query.map(i => <div key={i.id} className="Sales__table-item">
+                <Sale {...i.discount} disabled={i.quantity === 0}
+                      className="Sales__badge"/>
+                <SaleProduct
+                    name={i.name}
+                    price={i.price}
+                    producer={i.producer}
+                    discount={i.discount}
+                    pictures={i.pictures}
+                    quantity={i.quantity}
+                    onClick={() => (history.push(`/product/${i.SKU}`))}
+                    onAdd={this.handleAdd(i)}
+                    className="Sales__table-item__product"
+                />
+              </div>)
+            }
+          </div>
+          <PaginationView query={query} className="Sales__pagination"/>
 
-        <div className="Sales__info">
-          <Title>Скидки</Title>
-          <p>
-            В магазине действует специальная система скидок, которая зависит от суммы покупки.  Скидка рассчитывается автоматически в корзине покупок при оформлении заказа.
-          </p>
-          <p>
-            <h2>
-              При покупке на сумму:
-            </h2>
-            <ul className="list-unstyled">
-              <li>от    6000 руб. - 2,5%</li>
-              <li>от 10 000 руб. -   5%</li>
-              <li>от 20 000 руб. - 7,5%</li>
-            </ul>
-          </p>
-          <p>
-              На товары участвующие в распродаже система скидок не распространяется, и при  совместном заказе с другими товарами их стоимость не учитывается в общей сумме заказа для расчета скидки.
-          </p>
-          <p>
-            В один заказ не может быть включено более двух товаров участвующих в распродаже. На второй заказ,  товара по распродаже, в течении 24 часов не распространяется бесплатная доставка.
-          </p>
+          <div className="Sales__info">
+            <Title>Скидки</Title>
+            <p>
+              В магазине действует специальная система скидок, которая зависит
+              от суммы покупки. Скидка рассчитывается автоматически в корзине
+              покупок при оформлении заказа.
+            </p>
+            <p>
+              <h2>
+                При покупке на сумму:
+              </h2>
+              <ul className="list-unstyled">
+                <li>от 6000 руб. - 2,5%</li>
+                <li>от 10 000 руб. - 5%</li>
+                <li>от 20 000 руб. - 7,5%</li>
+              </ul>
+            </p>
+            <p>
+              На товары участвующие в распродаже система скидок не
+              распространяется, и при совместном заказе с другими товарами их
+              стоимость не учитывается в общей сумме заказа для расчета скидки.
+            </p>
+            <p>
+              В один заказ не может быть включено более двух товаров участвующих
+              в распродаже. На второй заказ, товара по распродаже, в течении 24
+              часов не распространяется бесплатная доставка.
+            </p>
+          </div>
         </div>
-      </div>
     );
   }
 }
