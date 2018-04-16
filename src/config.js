34ec061dc9610f34f78287bbcbc8218df8a9bde9
denis zaytcev <d.zaycev@software2life.com>
2018-04-16 08:08:37 +0300

    домен

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const port = process.env.PORT || 8000;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
 
-export const apiURL = process.env.REACT_APP_API_URL || 'http://95.213.236.60:8082';
+export const apiURL = process.env.REACT_APP_API_URL || 'http://darkwaters.store:8082';
 export const apiBasePath = '/api/v1';
 
