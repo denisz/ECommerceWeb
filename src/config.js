9148aeb16565ecbb786653cc06afaa46718bf8ed
denis zaytcev <d.zaycev@software2life.com>
2018-05-06 13:06:25 +0300

    config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const port = process.env.PORT || 8000;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
 
-export const apiURL = process.env.REACT_APP_API_URL || 'http://darkwaters.store:8082';
+export const apiURL = process.env.REACT_APP_API_URL || 'https://darkwaters.store:8082';
 export const apiBasePath = '/api/v1';
 
 export const corpEmail = 'admin@darkwaters.store';
